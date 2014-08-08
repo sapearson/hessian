6c731b0a4476eae8a8ce5c6f4f061ff6e3d959b0
Sarah Pearson <spearson@thalassa.(none)>
2014-08-08 14:09:22 -0400

    Updated with McGill&Binney1990

--- hessian_calc.py
+++ hessian_calc.py
@@ -20,11 +20,14 @@ from streamteam.potential import IsochronePotential
 import logging
 from astropy import log as logger
 logger.setLevel(logging.DEBUG)
+from astropy.constants import G
+
+usys = (u.kpc, u.Myr, u.Msun)
+G = G.decompose(usys).value
 
 cache_path = "/home/spearson/Hessian/stream-team/hessian"
 
-#np.random.seed(42)
-#Current issues: it uses the orbit integration function in several steps. I could write that smarter.
+#np.random.seed(42)#Current issues: it uses the orbit integration function in several steps. I could write that smarter.
 
 
 #------------------------------------------------Step 1----------------------------------------------------------#
@@ -84,54 +87,95 @@ def grid_of_AA(w0):
     return action_array,angle_array
 
 
-def grid_of_xv(w0):
-    """The function computes grid of x,y,z,vx,vy,vz around initial inputted orbit.
-    The grid is computed in action/angle space around the J,theta for the particular orbit using the 'grid_of_AA() function',
-    and then f(w) finds the x,y,z,vx,vy,vz grid, which is outputted."""
+def spol_to_cart(r,p,t,p_r,p_p,p_t):
+    """Performs coordinate transformation from spherical                                  
+    polar coordinates with (r,phi,theta,p_r,p_phi,p_theta)                                
+    having usual meanings to cartesian coordinates."""
+    x1 = r*np.sin(t)*np.cos(p)
+    x2 = r*np.sin(t)*np.sin(p)
+    x3 = r*np.cos(t)
+    v1 = np.cos(t)*np.cos(p)*p_r + r*np.cos(t)*np.sin(p)*p_p + r*np.sin(t)*np.sin(p)*p_t
+    v2 = np.cos(t)*np.sin(p)*p_r - r*np.cos(t)*np.cos(p)*p_p + r*np.sin(t)*np.sin(p)*p_t
+    v3 = np.sin(t)*p_r - r*np.cos(t)*p_t
 
-    actions,angles,freqs,M,b,Iso = find_J_theta(w0)
-#First we want to go from our J,theta grid to x,v so we can run orbits in LM10
-# function to minimize f to get w = (x,v)
-    action_array,angle_array = grid_of_AA(w0)
-    def f(w,action_array,angle_array):
-        try:
-            J_prime,theta_prime = Iso.action_angle(w[:3],w[3:])
-        except ValueError: # unbound                                                                                                                                                                                                                                                
-            return np.zeros_like(w) + 1e6
-        return np.squeeze(np.hstack((action_array,angle_array)) - np.hstack((J_prime,theta_prime)))
+    return np.array([x1,x2,x3,v1,v2,v3])
 
-# We have to try different initial conditions for w to compute J_prime and theta_prime
-    n = len(action_array)
-    n=2
-    xv_coordinates = []
-    for k in range(n):
-        root_w = []
-        for i in range(100):
-            w0 = np.append(np.random.random(size=3)*10, np.random.random(size=3)/10.)
-            s = so.root(f,w0,args=(action_array[k,:],angle_array[0,:]))
-            root_w.append(s.x)    #what is x?
-        root_w = np.array(root_w)
-        xv_coordinates.append(np.median(root_w,axis=0)) #median of all the output from f(w)
-#The array below is what we want for initial conditions for the new orbits from grid
-    xv_coordinates = np.array(xv_coordinates)   # we want array instead of list
-    return xv_coordinates
+def angact_to_xv_iso(act,ang,M,b):                                                        
+    """Calculate x and v for a given set of agtion angles using the analytically          
+    solvable isochrone potential with best fit parameters b and M.                        
+    Function takes in array of action and angles (1x3,1x3) and the best fit parameters for b         
+    and M in the isocrhone potential and returns x1,x2,x3,v1,v2,v3 for those action       
+    angles.                                                                               
+    We use Appendix 2 in McGill&Binney 1990
+    J1,2,3 = Jr,phi,theta = Jr,Lz,L-Lz"""                                            
 
+    k = G*M                                                                               
+    Lz = act[0,1]                    #This is J2                                                     
+    L = act[0,2]+Lz                 #J3+J2 (A8)                                              
+    l_s = np.sqrt(1-(Lz**2/L**2))        #A9   
+    H = (-2*k**2)/(2*act[0,0]+L+np.sqrt(4*b*k+L**2))**2     #A7                                 
+    a = -k/(2*H)-b                       #A10                                             
+    e = np.sqrt(1+L**2/(2*H*a**2))       #A11                                             
+    omega = np.sqrt(k)/(2*(a+b)**(3/2)) * (1+L/(np.sqrt(4*b*k+L**2)))                    
+    
+    def psi_func(x,a,e,b,ang):
+        """This function is defined to solve for psi in A12
+        It takes in an array of initial guesses for psi (x),a,e,b,ang
+        and returns function A12 for which we can find the roots."""
+        return x - a*e/(a+b)*np.sin(x) - ang[0,0]  #A12: Function to get psi                          
+    x = np.pi/2.
+    sol = so.root(psi_func,x, (a,e,b,ang))
+    psi = sol.x
+    
+    r = a*np.sqrt((1-e*np.cos(psi))*(1-e*np.cos(psi)+2*b/a)) #A13                            
+    Gamma = np.sqrt((a+b)/k)*((a+b)*psi-a*e*np.sin(psi))  # A16                           
+    Lambda = np.arctan(np.sqrt((1+e)/(1-e))*np.tan(0.5*psi)) + L/(np.sqrt(L**2+4*b*k))*np.arctan(np.sqrt((a*(1+e)+2*b)/(a*(1-e)+2*b))*np.tan(0.5*psi))   #A17
+    chi = ang[0,2] - omega*Gamma + Lambda     #A13                                              
+    theta = np.arcsin(l_s*np.sin(chi))# A14                                              
+    u_f = np.arcsin(1/np.tan(theta)*np.sqrt(1/l_s**2-1)) # we need this to find phi (Sander's code)
+    phi = ang[0,1]+u_f-np.sign(Lz)*ang[0,2]  #ang2, ang3
+    p_r = np.sqrt(k/(a+b))*(a*e*np.sin(psi))/r    #A15                                    
+    p_t = L*l_s*np.cos(chi)/np.cos(phi)      #A15                                                                                                  p_p = Lz                                                                              
+    
+    # We now need to convert from spherical polar coord to cart. coord.                   
+    xv_coordinates = spol_to_cart(r,phi,theta,p_r,p_p,p_t)                       
+    xv_coordinates = np.squeeze(xv_coordinates)
+    
+    return xv_coordinates                                           
+
+#w0=[8.161671207, 0.224760075, 16.962073974, -0.05826389, -0.10267707,-0.00339917]
+#act,ang =grid_of_AA(w0)
+#a,an,fr,M,b,Iso = find_J_theta(w0)
+#xv_cootcinates,psi = angact_to_xv_iso(act,ang,M,b)
+#print psi
+
+#def grid_of_xv(w0):
+ #   """Loop over function above so we ge grid of x,v not just x,v"""
+  #  act,ang = grid_of_AA(w0)   #125x3 and 1x3
+   # n = len(act)
+    #n = 2
+   # for k in range(n):
+    #    x_v_coordinates = angact_to_xv_iso(act,ang,M,b)
+        
 #-----------------------------------------------Step 3-----------------------------------------------------------#
 #Integrate orbits for all (x,v) on grid in LM10
 #xv_coordinates are our new initial conditions in usys.
 # Compute "true" (J,theta) for all orbits using Sander's
 #Basically same step as step 1, but loop over all new orbits
 def grid_of_J_theta_orbit(w0):
-    """We now get action/angle grid for desired orbit. This will be used to calculate the Hessian for this orbit."""
-    actions,angles,freqs,M,b,Iso = find_J_theta(w0)
+    """We now get action/angle grid for desired orbit (in the arbitrary potential).
+    This will be used to calculate the Hessian for this orbit."""
+    actions,angles,freqs,M,b,Iso = find_J_theta(w0) #actually just do this to get M,b
+    act,ang = grid_of_AA(w0) #get grid of action angles
     params = []
-    xv_coordinates = grid_of_xv(w0)
-    n=2
+    xv_coordinates = angact_to_xv_iso(act,ang,M,b) #get grid of x,v from grid of action agnles
+    
+    n=1
     acceleration = lambda t, *args: Iso.acceleration(*args)
     integrator = si.LeapfrogIntegrator(acceleration)
     for k in range(n):
     #    xv_coordinates, Iso = grid_of_xv(w0)
-        w0 = xv_coordinates[k,:] #These are our initial conditions for all the new orbits
+        w0 = xv_coordinates[:] #These are our initial conditions for all the new orbits
         t,w = integrator.run(w0, dt=1., nsteps=100000) #we integrate each of these orbits to get J,theta for each orbit
         usys = (u.kpc, u.Myr, u.Msun) #Our init system                                                                            
         phase_space = np.squeeze(w)
@@ -154,11 +198,12 @@ def intorpolate_J_theta_grid():
 
 def hessian_freq():
     """Calculaste Hessian based on derivative of frequencies with respect to J for given orbit"""
-
+    # We will have interpolated frequencies uniform in action/angle space for arbitrary potential.
+    # our desired value is the central point of J,theta,omega in this grid?
 
 def freq_check():
     """Check that the derivative of the hamiltonian with respect to J yields the frequencies obtained with Sanders"""
-
+    #isocrhone in cartesian coordinates
 
 
 #----------------------------------------------Step 7---------------------------------------------#
@@ -179,5 +224,4 @@ action_array,angle_array = grid_of_AA(w0)
 print '---------New actions from x,v -> J, theta----------'
 print actions                                                                                                                             
 print '---------Inputted actions from grid----------'                                                                                     
-#action_array,angle_array = grid_of_AA(w0)
 print action_array[:2,:]
